b1348c0d3e2753deaf5f2e120f9b40d923ee6193
Qasim Hafeez <qasimh556@gmail.com>
2020-03-22 13:20:51 +0400

    added contact model

---
+++ models/Contact.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users"
+  },
+  name: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String
+  },
+  phone: {
+    type: String
+  },
+  Type: {
+    type: String,
+    default: "personal"
+  },
+  Date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("contact", ContactSchema);
