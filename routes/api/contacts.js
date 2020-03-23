139c88e8c8259671ae3a8a191e266a885c57a50d
Qasim Hafeez <qasimh556@gmail.com>
2020-03-23 10:50:00 +0400

    removed bugs

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,4 +1,60 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const { check, validationResult } = require("express-validator/check");
+
+// Loading models
+const User = require("../../models/User");
+const Contact = require("../../models/Contact");
+
+// @route   GET api/contacts
+// @desc    Get all contacts of a particular user
+// @access  Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// @route   POST api/contacts
+// @desc    Add a contact
+// @access  Private
+router.post(
+  "/",
+  auth,
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id
+      });
+
+      const contact = await newContact.save();
+      res.json(contact);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
