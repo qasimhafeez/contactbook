aa9de063d8f1534c9c0e6276133fcd15de79a6d1
qasimhafeez <qasimh556@gmail.com>
2020-04-19 15:48:09 +0400

    initial commit

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h3>About this App</h3>
+      <p className="my-1">You can save you contacts here.</p>
+    </div>
+  );
+};
+
+export default About;
